1cd4613205418ec41c0ac87289b8c6bdd023b6c5
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2025-06-26 15:28:28 -0300

    Update init.lua add oil

--- init.lua
+++ init.lua
@@ -100,7 +100,8 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
+-- Enter oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
